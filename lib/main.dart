54f6c028e6b33e151d200adf7fe90129cd0eb3d6
Mikkel <mikkel@convai.dk>
2024-11-04 19:37:30 +1100

    Added dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,17 @@ import 'package:tumblelog/features/monitoring/domain/usecases/load_skills.dart';
 import 'package:tumblelog/features/monitoring/presentation/blocs/monitor_bloc/monitor_bloc.dart';
 import 'package:tumblelog/injection_container.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
+
   WidgetsFlutterBinding.ensureInitialized();
 
   // TODO: Hide keys
   await Supabase.initialize(
-    url: 'https://iszxmsufbsisxdelhpvb.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlzenhtc3VmYnNpc3hkZWxocHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk5MzM0MDksImV4cCI6MjA0NTUwOTQwOX0.VhjVvMTZEQ06x-ODMEjaCiU0VQo0xxPdLseHoXI-Myg',
+    url: dotenv.env['DB_URL']!,
+    anonKey: dotenv.env['DB_KEY']!,
   );
 
   setupLocator();
