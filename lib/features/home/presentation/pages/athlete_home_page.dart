4c083b3b21a0d5e4c5d65ef91c1908070d5e3d53
Mikkel <mikkel@convai.dk>
2024-11-13 15:27:00 +1100

    Added naming for athlete

--- lib/features/home/presentation/pages/athlete_home_page.dart
+++ lib/features/home/presentation/pages/athlete_home_page.dart
@@ -8,10 +8,18 @@ import 'package:tumblelog/features/monitoring/presentation/pages/monitor_page.da
 import 'package:tumblelog/features/monitoring/presentation/pages/monitor_week_page.dart';
 import 'package:tumblelog/features/tracking/presentation/blocs/layout_cubit/layout_cubit.dart';
 import 'package:tumblelog/features/tracking/presentation/pages/session_page.dart';
+import 'package:tumblelog/injection_container.dart';
 import 'package:uuid/uuid.dart';
 
-class AthleteHomePage extends StatelessWidget {
+class AthleteHomePage extends StatefulWidget {
   const AthleteHomePage({super.key});
+
+  @override
+  State<AthleteHomePage> createState() => _AthleteHomePageState();
+}
+
+class _AthleteHomePageState extends State<AthleteHomePage> {
+  final TextEditingController _nameController = TextEditingController();
 
   void _startSession(
       BuildContext context, String athleteId, String athleteName) {
@@ -61,6 +69,33 @@ class AthleteHomePage extends StatelessWidget {
     );
   }
 
+  // TODO: Make Clean Architecture
+  Future<void> _updateUserName(
+      BuildContext context, String newName, String userId) async {
+    if (newName.isEmpty) return;
+
+    try {
+      final updatedUser = await supabaseClient
+          .from('users')
+          .update({'name': newName})
+          .eq('id', userId)
+          .select();
+
+      if (updatedUser.isNotEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Name updated successfully')),
+        );
+
+        // Update the AuthBloc state if necessary
+        context.read<AuthBloc>().add(CheckAuthStatus());
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Failed to update name')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final authState = context.watch<AuthBloc>().state;
@@ -78,6 +113,65 @@ class AthleteHomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              athleteName == "NoName"
+                  ? Center(
+                      child: Card(
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        margin: const EdgeInsets.all(16),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Please enter your name:',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    TextField(
+                                      controller: _nameController,
+                                      decoration: const InputDecoration(
+                                        border: OutlineInputBorder(),
+                                        labelText: 'Name',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              ElevatedButton(
+                                onPressed: () {
+                                  _updateUserName(
+                                    context,
+                                    _nameController.text.trim(),
+                                    athleteId,
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 24,
+                                    vertical: 16,
+                                  ),
+                                ),
+                                child: const Text('Save'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  : const SizedBox.shrink(),
+              const SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () => _startSession(context, athleteId, athleteName),
                 child: const Text('Start Session'),
